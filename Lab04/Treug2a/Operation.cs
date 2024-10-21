e6b492146ad655073a1d36d46ed409dcb71fae36
gavrun <38539655+gavrun@users.noreply.github.com>
2024-10-21 17:58:57 +0100

    Still in progress

---
+++ Lab04/Treug2a/Operation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Treug2a
+{
+    internal class Operation
+    {
+        // площадь треугольника по формуле Герона
+        public static double CalcArea(double a, double b, double c)
+        {
+            if ((a + b <= c) || (b + c <= a) || (a + c <= b)) //inverted
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("A triangle with such sides cannot be made.");
+                Console.ResetColor();
+                Console.WriteLine("Examples of valid triangles:\n" +
+                                        "Equilateral: 3, 3, 3\n" +
+                                        "Isosceles: 5, 5, 8\n" +
+                                        "Right-angled: 3, 4, 5\n" +
+                                        "Scalene: 6, 7, 8");
+                return 0;
+            }
+
+            double p = (a + b + c) / 2;
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+
+        }
+
+        // перегрузка площади для равностороннего треугольника
+        public static double CalcArea(double a)
+        {
+            return (a * a * Math.Sqrt(3)) / 4;
+        }
+
+        // условие неравенства треугольника ERR
+        //private static bool IsTriangleValid(double a, double b, double c)
+        //{
+        //    return (a + b > c) && (b + c > a) && (a + c > b);
+        //}
+    }
+}
---
+++ Lab04/Treug2a/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Treug2a
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Do you want to calculate the area of ​​an equilateral triangle? (y/n)");
+            string isEq = Console.ReadLine();
+
+            Console.WriteLine("Well..");
+
+            if (isEq.ToLower() == "y")
+            {
+                Console.WriteLine("Enter the length of the triangle side:");
+                double a = double.Parse(Console.ReadLine());
+
+                double s = Operation.CalcArea(a);
+
+                //небольшая таблица
+                Console.WriteLine("---------------------------");
+                Console.WriteLine("| Side    | Area    |");
+                Console.WriteLine("---------------------------");
+                Console.WriteLine("| {0:F2}    | {1:F2}    |", a, s);
+                Console.WriteLine("---------------------------");
+
+            }
+            else
+            {
+                Console.WriteLine("Enter the length of the First side:");
+                double a = double.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the length of the Second side:");
+                double b = double.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the length of the Third side:");
+                double c = double.Parse(Console.ReadLine());
+
+                double s = Operation.CalcArea(a, b, c);
+
+                if (s > 0)
+                {
+                    // еще одна небольшая таблица
+                    Console.WriteLine("---------------------------");
+                    Console.WriteLine("| Sides     | Area      |");
+                    Console.WriteLine("---------------------------");
+                    Console.WriteLine("| {0:F2}    | {1:F2}    |", a, s);
+                    Console.WriteLine("| {0:F2}    |           |", b);
+                    Console.WriteLine("| {0:F2}    |           |", c);
+                    Console.WriteLine("---------------------------");
+                }
+            }
+        }
+    }
+}
