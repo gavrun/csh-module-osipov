4d2abfdaa8cd4760beb0242fd4f4f53d5cb6800d
gavrun <38539655+gavrun@users.noreply.github.com>
2024-10-29 15:14:51 +0000

    Added project

---
+++ Lab07/Books/Book.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Books
+{
+    internal class Book : IComparable
+    {
+        // book fields
+        private string author;
+        private string title;
+        private int pages;
+        private int year;
+
+        // constructor could be helping a lot
+
+        // set fields
+        public void SetBook(string author, string title, int pages, int year)
+        {
+            this.author = author;
+            this.title = title;
+            this.pages = pages;
+            this.year = year;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("Book: Author: {0}, Title: {1}, Year: {2}, Pages: {3}", author, title, year, pages);
+        }
+
+        public int CompareTo(object obj)
+        {
+            //Book it = (Book)obj;
+            Book currentBook = obj as Book;
+
+            // null check 
+            if (currentBook == null)
+                throw new ArgumentException();
+
+            if (this.year == currentBook.year)
+            {
+                return 0;
+            } 
+            else if (this.year > currentBook.year)
+            {
+                return 1;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+    }
+}
---
+++ Lab07/Books/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Books
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // could be much easier 
+
+            //Book[] books;
+            //books = new Book[]
+            //{
+            //    new Book { author = "Fyodor Dostoevsky", title = "Crime and Punishment", pages = 671, year = 1866 },
+            //    new Book { author = "Leo Tolstoy", title = "War and Peace", pages = 1225, year = 1869 },
+            //    new Book { author = "Alexander Pushkin", title = "Eugene Onegin", pages = 320, year = 1833 },
+            //    new Book { author = "Anton Chekhov", title = "The Cherry Orchard", pages = 104, year = 1903 },
+            //    new Book { author = "Nikolai Gogol", title = "Dead Souls", pages = 384, year = 1842 }
+            //};
+
+            //declare and pop 5 books by method
+            Book[] books = new Book[]
+            {
+                new Book(),
+                new Book(),
+                new Book(),
+                new Book(),
+                new Book()
+            };
+            books[0].SetBook("Fyodor Dostoevsky", "Crime and Punishment", 671, 1866);
+            books[1].SetBook("Leo Tolstoy", "War and Peace", 1225, 1869);
+            books[2].SetBook("Alexander Pushkin", "Eugene Onegin", 320, 1833);
+            books[3].SetBook("Anton Chekhov", "The Cherry Orchard", 104, 1903);
+            books[4].SetBook("Nikolai Gogol", "Dead Souls", 384, 1842);
+
+            // list books on the shelf
+            foreach (var item in books)
+            {
+                item.Show();
+            }
+
+            // sort books
+            Array.Sort(books);
+            Console.WriteLine();
+
+            // list ordered books
+            foreach (var item in books)
+            {
+                item.Show();
+            }
+        }
+    }
+}
