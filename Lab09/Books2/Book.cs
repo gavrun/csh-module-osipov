bf3464cb9cac4fa019ffd575d114f54701da84de
gavrun <38539655+gavrun@users.noreply.github.com>
2024-10-30 11:26:12 +0000

    In progress

---
+++ Lab09/Books2/Book.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Books2
+{
+    internal class Book
+    {
+        private string author;
+        private string title;
+        private string publisher;
+        private int pages;
+        private int year;
+
+        private static double price = 9;
+
+        public Book() // default ctor
+        { }
+
+        static Book() // static ctor
+        {
+            price = 10;
+        }
+
+        public Book(string author, string title, string publisher, int pages, int year)
+        {
+            this.author = author;
+            this.title = title;
+            this.publisher = publisher;
+            this.pages = pages;
+            this.year = year;
+        }
+
+        public static void SetPrice(double price)
+        {
+            Book.price = price;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("\nКнига:\n Автор: {0}\n Название: {1}\nГод издания: { 2}\n {3}стр.\n Стоимость аренды: {4}", author, title, year, pages, Book.price);
+        }
+
+        public double PriceBook(int s)
+        {
+            double cust = s * price;
+            return cust;
+        }
+
+    }
+}
---
+++ Lab09/Books2/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Books2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book b2 = new Book("Толстой Л.Н.", "Война и мир", "Наука", 823, 2013);
+            b2.Show();
+
+            Book.SetPrice(12);
+            b2.Show();
+
+        }
+    }
+}
