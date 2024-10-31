71a42c4c3514345d6b9135d8152197bb95a90283
gavrun <38539655+gavrun@users.noreply.github.com>
2024-10-31 00:03:25 +0000

    Added project

---
+++ Lab11/Distance3/Distance.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Distance3
+{
+    internal class Distance
+    {
+        public int Feet;
+        public double Inches;
+
+        // default to zero
+        public Distance()
+        {
+            Feet = 0;
+            Inches = 0.0;
+        }
+
+        // take values and reduce to normal form
+        public Distance(int feet, double inches)
+        {
+            Feet = feet;
+            Inches = inches;
+
+            Feet = Feet + (int)(Inches / 12);
+            Inches %= 12;
+        }
+
+        // format
+        public override string ToString()
+        {
+            return $"{Feet}' - {Inches}\"";
+        }
+
+        // overload plus minus
+        public static Distance operator +(Distance distance1, Distance distance2)
+        {
+            return new Distance(distance1.Feet + distance2.Feet, distance1.Inches + distance2.Inches); // ctor handles normal form, etc.
+        }
+        public static Distance operator -(Distance distance1, Distance distance2)
+        {
+            int feet = distance1.Feet - distance2.Feet;
+            double inches = distance1.Inches - distance2.Inches;
+            return new Distance(feet, inches);
+        }
+
+        // overload all equality comparison
+        public static bool operator ==(Distance distance1, Distance distance2)
+        {
+            return distance1.Feet == distance2.Feet && Math.Abs(distance1.Inches - distance2.Inches) < 0.001; // math tolerance for fractional inches
+
+        }
+        public static bool operator !=(Distance distance1, Distance distance2)
+        {
+            return !(distance1 == distance2);
+        }
+        //public override bool Equals(object obj)
+        //public override int GetHashCode()
+        public static bool operator >(Distance distance1, Distance distance2)
+        {
+            return distance1.Feet > distance2.Feet || (distance1.Feet == distance2.Feet && distance1.Inches > distance2.Inches);
+        }
+        public static bool operator <(Distance distance1, Distance distance2)
+        {
+            return distance1.Feet < distance2.Feet || (distance1.Feet == distance2.Feet && distance1.Inches < distance2.Inches);
+        }
+        public static bool operator >=(Distance distance1, Distance distance2)
+        {
+            return distance1 > distance2 || distance1 == distance2;
+        }
+        public static bool operator <=(Distance distance1, Distance distance2)
+        {
+            return distance1 < distance2 || distance1 == distance2;
+        }
+    }
+}
---
+++ Lab11/Distance3/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Distance3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Distance distance0 = new Distance();
+            Distance distance1 = new Distance(1, 5.5);
+            Distance distance2 = new Distance(3, 19.5);
+
+            Distance distanceSum = distance1 + distance2;
+            Distance distanceDiff = distance1 - distance2;
+
+            Console.WriteLine("Distance 0: " + distance0);
+            Console.WriteLine("Distance 1: " + distance1);
+            Console.WriteLine("Distance 2: " + distance2);
+            Console.WriteLine("Sum 1+2: " + distanceSum);
+            Console.WriteLine("Subtraction 1-2: " + distanceDiff);
+
+            Console.WriteLine("Compare 1<2: " + (distance1 < distance2));
+            Console.WriteLine("Compare 1<=2: " + (distance1 <= distance2));
+            Console.WriteLine("Compare 1>=2: " + (distance1 > distance2));
+            Console.WriteLine("Compare 1>=2: " + (distance1 >= distance2));
+        }
+    }
+}
